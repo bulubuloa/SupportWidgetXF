c076688cd3da07e371f6d00672bc31ba68b4cc7e
Hoang Quach <hoang.quach@asiaesolutions.com>
2018-10-05 11:53:33 +0700

    update baseviewmodel

--- SupportWidgetXF/ViewModels/BaseViewModel.cs
+++ SupportWidgetXF/ViewModels/BaseViewModel.cs
@@ -1,13 +1,164 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using SupportWidgetXF.Models;
 using Xamarin.Forms;
 
 namespace SupportWidgetXF.ViewModels
 {
-    public abstract class BaseViewModel : BindableObject
+    public interface IHandleViewAppearing
     {
+        Task OnViewAppearingAsync(VisualElement view);
+    }
+
+    public interface IHandleViewDisappearing
+    {
+        Task OnViewDisappearingAsync(VisualElement view);
+    }
+
+    public abstract class BaseViewModel : BindableObject, IHandleViewDisappearing, IHandleViewAppearing
+    {
+        /*
+         * Validations
+         */
+        private string _WarningMessage;
+        public string WarningMessage
+        {
+            get => _WarningMessage;
+            set
+            {
+                _WarningMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _IsValidThisForms;
+        public bool IsValidThisForms
+        {
+            get => _IsValidThisForms;
+            set
+            {
+                _IsValidThisForms = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        /*
+         * View Status
+         */
+        private bool _IsInitialize;
+        public bool IsInitialize
+        {
+            get => _IsInitialize;
+            set
+            {
+                _IsInitialize = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _IsRefresh;
+        public bool IsRefresh
+        {
+            get => _IsRefresh;
+            set
+            {
+                _IsRefresh = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand RefreshScreenCommand => new AsyncCommand(OnRefreshScreenCommand);
+        public virtual Task OnRefreshScreenCommand()
+        {
+            return Task.FromResult(true);
+        }
+
+        protected void DebugMessage(string content, string title = "")
+        {
+            Debug.WriteLine(title + ": " + content);
+        }
+
+        protected virtual async void RunActionOnNewThread(Func<Task> action, CancellationTokenSource cancellationToken, bool IsSyncBusy = false, int timeDelay = 100)
+        {
+            try
+            {
+                if(IsSyncBusy)
+                    IsBusy = true;
+
+                await Task.Delay(timeDelay);
+                await Task.Run(action, cancellationToken.Token);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                if (IsSyncBusy)
+                    IsBusy = false;
+            }
+        }
+
+        protected virtual async void RunActionOnNewThread(Action action, bool IsSyncBusy = false, int timeDelay = 100)
+        {
+            try
+            {
+                if (IsSyncBusy)
+                    IsBusy = true;
+
+                await Task.Delay(timeDelay);
+                action();
+            }
+            catch (Exception ex)
+            {
+                DebugMessage(ex.StackTrace);
+            }
+            finally
+            {
+                if (IsSyncBusy)
+                    IsBusy = false;
+            }
+        }
+
+        protected virtual async void RunActionOnNewThread(Func<Task> action, bool IsSyncBusy = false, int timeDelay = 100)
+        {
+            try
+            {
+                if (IsSyncBusy)
+                    IsBusy = true;
+
+                await Task.Delay(timeDelay);
+                await action();
+            }
+            catch (Exception ex)
+            {
+                DebugMessage(ex.StackTrace);
+            }
+            finally
+            {
+                if (IsSyncBusy)
+                    IsBusy = false;
+            }
+        }
+
         public BaseViewModel()
         {
+
         }
 
         public virtual Task InitializeAsync(object navigationData)
@@ -15,5 +166,14 @@ namespace SupportWidgetXF.ViewModels
             return Task.FromResult(false);
         }
 
+        public virtual Task OnViewAppearingAsync(VisualElement view)
+        {
+            return Task.FromResult(true);
+        }
+
+        public virtual Task OnViewDisappearingAsync(VisualElement view)
+        {
+            return Task.FromResult(true);
+        }
     }
 }
