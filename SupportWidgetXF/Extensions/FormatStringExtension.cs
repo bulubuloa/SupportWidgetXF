1f24ca98fe3ff73190cd96e7f1a2966fa2130f1a
Thien Nhan Nguyen Thanh <thiennhan.nguyenthanh@asiaesolutions.com>
2018-11-22 16:15:44 +0700

    update

---
+++ SupportWidgetXF/Extensions/FormatStringExtension.cs
@@ -0,0 +1,29 @@
+﻿namespace SupportWidgetXF.Extensions
+{
+    public static class FormatStringExtension
+    {
+        public static string UppercaseWords(this string value)
+        {
+            char[] array = value.ToCharArray();
+            if (array.Length >= 1)
+            {
+                if (char.IsLower(array[0]))
+                {
+                    array[0] = char.ToUpper(array[0]);
+                }
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] == ' ')
+                {
+                    if (char.IsLower(array[i]))
+                    {
+                        array[i] = char.ToUpper(array[i]);
+                    }
+                }
+            }
+            return new string(array);
+        }
+    }
+}
