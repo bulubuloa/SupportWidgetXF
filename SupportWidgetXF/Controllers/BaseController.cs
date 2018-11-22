d3b0e971ebe3b98ed42bebd82bad8ba8136e7ca6
Hoang Quach <hoang.quach@asiaesolutions.com>
2018-11-22 16:20:55 +0700

    add base controller

---
+++ SupportWidgetXF/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SupportWidgetXF.Controllers
+{
+    public abstract class BaseController
+    {
+        public void DebugMessage(string content, string prefix = "")
+        {
+            Debug.WriteLine(prefix + ": " + content);
+        }
+
+        public BaseController()
+        {
+        }
+    }
+}
